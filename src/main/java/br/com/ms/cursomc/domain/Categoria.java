154c9a20b0a36542aab42a0aa0a1e599a1891248
Miqueias Pires <miqueiasousa011@gmail.com>
2020-07-03 10:28:08 -0300

    Associação Produto Categoria

--- src/main/java/br/com/ms/cursomc/domain/Categoria.java
+++ src/main/java/br/com/ms/cursomc/domain/Categoria.java
@@ -2,11 +2,25 @@ package br.com.ms.cursomc.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @ManyToMany
+    @JoinTable(
+            name = ("CATEGORIA_PRODUTO"),
+            joinColumns = @JoinColumn(name = ("categoria_id")),
+            inverseJoinColumns = @JoinColumn(name = (("produto_id")))
+    )
+    private List<Produto> produtos = new ArrayList<>();
 }
--- src/main/java/br/com/ms/cursomc/domain/Produto.java
+++ src/main/java/br/com/ms/cursomc/domain/Produto.java
@@ -2,12 +2,21 @@ package br.com.ms.cursomc.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@Entity
 public class Produto implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private Double preco;
+
+    @ManyToMany(mappedBy = "produtos")
+    private List<Categoria> categorias = new ArrayList<>();
 }
