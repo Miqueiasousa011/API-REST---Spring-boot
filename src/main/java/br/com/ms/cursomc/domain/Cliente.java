6da17197095ab4f54f54a432e721f3dd54e423a9
Miqueias Pires <miqueiasousa011@gmail.com>
2020-07-03 10:54:17 -0300

    Associação Cliente Telefone

--- src/main/java/br/com/ms/cursomc/domain/Cliente.java
+++ src/main/java/br/com/ms/cursomc/domain/Cliente.java
@@ -3,17 +3,24 @@ package br.com.ms.cursomc.domain;
 import br.com.ms.cursomc.domain.enums.TipoCliente;
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@Entity
 public class Cliente implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
     private String cpfOuCnpj;
     private TipoCliente tipoCliente;
+
+    @ElementCollection
+    @CollectionTable(name = ("TELEFONE"))
     private Set<String> telefone = new HashSet<>();
 }
