447b7ef03b2d9c96b4ff300568c5abbaa76178d1
Miqueias Pires <miqueiasousa011@gmail.com>
2020-07-03 11:01:44 -0300

    Associação Pedido Endereco

--- src/main/java/br/com/ms/cursomc/domain/Pedido.java
+++ src/main/java/br/com/ms/cursomc/domain/Pedido.java
@@ -2,12 +2,20 @@ package br.com.ms.cursomc.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
+@Entity
 public class Pedido implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Date instante;
+
+    @ManyToOne
+    @JoinColumn(name = ("enderecoEntrega_id"))
+    private Endereco enderecoDeEntrega;
 }
