fc2792fe0c23a4c7504669ae599007c663702b60
Miqueias Pires <miqueiasousa011@gmail.com>
2020-07-03 10:33:55 -0300

    Associação Estado Cidade

--- src/main/java/br/com/ms/cursomc/domain/Cidade.java
+++ src/main/java/br/com/ms/cursomc/domain/Cidade.java
@@ -2,11 +2,19 @@ package br.com.ms.cursomc.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
+@Entity
 public class Cidade implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = ("cidade_id"))
+    private Estado estado;
 }
--- src/main/java/br/com/ms/cursomc/domain/Estado.java
+++ src/main/java/br/com/ms/cursomc/domain/Estado.java
@@ -2,11 +2,20 @@ package br.com.ms.cursomc.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@Entity
 public class Estado implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @OneToMany(mappedBy = "estado")
+    private List<Cidade> cidades = new ArrayList<>();
 }
