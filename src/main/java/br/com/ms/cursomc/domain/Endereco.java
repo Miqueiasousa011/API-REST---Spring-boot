5eec968583b1837100d31fc2a30b3d61547c0d9f
Miqueias Pires <miqueiasousa011@gmail.com>
2020-07-03 10:49:48 -0300

    Associação Endereco Cidade

--- src/main/java/br/com/ms/cursomc/domain/Endereco.java
+++ src/main/java/br/com/ms/cursomc/domain/Endereco.java
@@ -2,15 +2,23 @@ package br.com.ms.cursomc.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
+@Entity
 public class Endereco implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String logradouro;
     private String numero;
     private String complemento;
     private String bairro;
     private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = ("cidade_id"))
+    private Cidade cidade;
 }
